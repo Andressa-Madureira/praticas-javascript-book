030802e057afaab61a3f49d69191a6649d241a57
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-11-22 14:01:20 -0300

    inclusão ex08

---
+++ ex08/script.js
@@ -0,0 +1,5 @@
+const dias = Number(prompt(" Número de dias: "))
+const horas = Number(prompt(" Número de horas: "))
+const duracaoTotal = (dias * 24) + horas 
+
+alert(`Total de horas: ${duracaoTotal}`)
