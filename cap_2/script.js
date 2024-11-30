65cdf04207c34bb5f41d5886e522bc989110645d
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-11-30 17:47:27 -0300

    incluindo preventDefault

--- cap_2/script.js
+++ cap_2/script.js
@@ -4,5 +4,6 @@ const resp = document.querySelector("h3")
 
 frm.addEventListener("submit", (e) => {
     const nome = frm.inNome.value // vai pegar no form o "valor=value" do input de texto nome
-    resp.innerHTML = `Olá ${nome}` //exibe a resposta do programa
+    resp.innerHTML = `Olá, ${nome}!` //exibe a resposta do programa
+    e.preventDefault() //impede que a página recarregue automaticamente
 })
