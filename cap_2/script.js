de25a2cbaeb39a0fb0ebed6109584ca47f3e504a
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-11-30 18:04:41 -0300

    Ajustando cor e formatação

--- cap_2/script.js
+++ cap_2/script.js
@@ -1,16 +1,13 @@
-
-const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
 
 frm.addEventListener("submit", (e) => {
-
-    
-    const nome = frm.inNome.value // vai pegar no form o "valor=value" do input de texto nome
-    resp.innerHTML = `Olá, ${nome}!` //exibe a resposta do programa
-    resp.style.color = "red" 
-    resp.style.background = "black" 
-    resp.style.width = "15%"
-    resp.style.textAlign = "center"
-    resp.style.borderRadius = "5px"
-    e.preventDefault() //impede que a página recarregue automaticamente
-})
+  const nome = frm.inNome.value; // vai pegar no form o "valor=value" do input de texto nome
+  resp.innerHTML = `Olá, ${nome}!`; //exibe a resposta do programa
+  resp.style.color = "red";
+  resp.style.background = "black";
+  resp.style.width = "15%";
+  resp.style.textAlign = "center";
+  resp.style.borderRadius = "5px";
+  e.preventDefault(); //impede que a página recarregue automaticamente
+});
