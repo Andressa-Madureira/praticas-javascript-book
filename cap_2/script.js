c00bf324f62b58dfbb3299c9a1dbcd5a2e086488
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-11-30 17:55:25 -0300

    estilizando

--- cap_2/script.js
+++ cap_2/script.js
@@ -3,7 +3,14 @@ const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
 frm.addEventListener("submit", (e) => {
+
+    
     const nome = frm.inNome.value // vai pegar no form o "valor=value" do input de texto nome
     resp.innerHTML = `Olá, ${nome}!` //exibe a resposta do programa
+    resp.style.color = "red" 
+    resp.style.background = "black" 
+    resp.style.width = "15%"
+    resp.style.textAlign = "center"
+    resp.style.borderRadius = "5px"
     e.preventDefault() //impede que a página recarregue automaticamente
 })
