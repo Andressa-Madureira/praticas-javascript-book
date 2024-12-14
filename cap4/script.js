d4c8b346f08b1e51a172fd12389bb2f359e8ec6c
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-14 09:04:39 -0300

    Cap 4 ex4_1

---
+++ cap4/script.js
@@ -0,0 +1,27 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h2")
+const resp2 = document.querySelector("h3")
+
+frm.addEventListener("submit", (e)=>{
+
+    const aluno = frm.inAluno.value
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+
+    const media = (nota1 + nota2) / 2
+    if(media >= 7){
+        resp1.innerText = `Média das Notas: ${media}`
+        resp2.innerText = `Parabéns, ${aluno}! Você foi aprovado(a) ! `
+        resp2.style.color = "blue"
+        
+    }else{
+        resp1.innerText = `Média das Notas: ${media}`
+        resp2.innerText = `Parabéns, ${aluno}! Você foi reprovado(a) ! `
+        resp2.style.color = "red"
+    }
+
+   
+    
+
+    e.preventDefault()
+})
