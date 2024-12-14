4715ab10f7b8b3def266e3f5f9a75a9be83e70e7
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-14 09:31:50 -0300

    Ajustes no exercicío ex4_1

--- cap4/script.js
+++ cap4/script.js
@@ -2,26 +2,26 @@ const frm = document.querySelector("form")
 const resp1 = document.querySelector("h2")
 const resp2 = document.querySelector("h3")
 
-frm.addEventListener("submit", (e)=>{
+frm.addEventListener("submit", (e) => {
 
     const aluno = frm.inAluno.value
     const nota1 = Number(frm.inNota1.value)
     const nota2 = Number(frm.inNota2.value)
 
     const media = (nota1 + nota2) / 2
-    if(media >= 7){
+    if (media >= 7) {
         resp1.innerText = `Média das Notas: ${media}`
-        resp2.innerText = `Parabéns, ${aluno}! Você foi aprovado(a) ! `
+        resp2.innerText = `Parabéns, ${aluno}! Você foi aprovado(a)! `
         resp2.style.color = "blue"
-        
-    }else{
+
+    } else if (media >= 4) {
         resp1.innerText = `Média das Notas: ${media}`
-        resp2.innerText = `Parabéns, ${aluno}! Você foi reprovado(a) ! `
+        resp2.innerText = `Atenção, ${aluno}! Você está em exame!`
+        resp2.style.color = "green"
+    } else {
+        resp1.innerText = `Média das Notas: ${media}`
+        resp2.innerText = `Ops, ${aluno}! Você foi reprovado(a)! `
         resp2.style.color = "red"
     }
-
-   
-    
-
     e.preventDefault()
 })
