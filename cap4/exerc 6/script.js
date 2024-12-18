af099ef4b5c99fbb57be3d123659e0a5583ff6a7
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-18 14:56:19 -0300

    Exercício 6 capítulo 4

---
+++ cap4/exerc 6/script.js
@@ -0,0 +1,8 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#resp1")
+const resp2 = document.querySelector("#resp2")
+const resp3 = document.querySelector("#resp3")
+
+frm.addEventListener("submit",(e)=>{
+    const valorSaque = Number(frm.inSaque.value)
+})
