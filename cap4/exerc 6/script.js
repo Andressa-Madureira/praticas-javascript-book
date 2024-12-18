681834991f7710bd48196a4367c8422cc7cca141
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-18 15:10:57 -0300

    Conclusão do exercício 6 cap 4

--- cap4/exerc 6/script.js
+++ cap4/exerc 6/script.js
@@ -4,5 +4,29 @@ const resp2 = document.querySelector("#resp2")
 const resp3 = document.querySelector("#resp3")
 
 frm.addEventListener("submit",(e)=>{
+    e.preventDefault()
     const valorSaque = Number(frm.inSaque.value)
+
+    if(valorSaque % 10 != 0){
+        alert("Valor inválido para notas disponíveis(10, 50 e 100)")
+        frm.inSaque.focus()
+        return
+    }
+
+    const notasCem = Math.floor(valorSaque / 100)  //calcular as notas de 100
+    let resto = valorSaque % 100 // quanto sobra para pagar
+
+    const notasCinquenta = Math.floor(resto / 50) //calcula as notas de 50
+    resto = resto % 50
+
+    const notasDez = Math.floor(resto / 10) 
+    if(notasCem > 0 ){
+        resp1.innerHTML = `Notas de R$ 100: ${notasCem}`
+    }
+    if(notasCinquenta > 0){
+        resp2.innerHTML = `Notas de R$ 50: ${notasCinquenta}`
+    }
+    if(notasDez > 0){
+        resp3.innerHTML = `Notas de R$ 10: ${notasDez}`
+    }
 })
