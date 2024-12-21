f4a395b33ca0c4e89f693288438840c08c5047a1
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-21 13:21:08 -0300

    concluído ativ 2 cap 4

--- cap4/ativ-extra/scriptex2.js
+++ cap4/ativ-extra/scriptex2.js
@@ -1,37 +1,18 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
-/*
+
 frm.addEventListener("submit", (e)=>{
     e.preventDefault()
-
+    
     const velPermitida = Number(frm.inPermitida.value)
     const velCondutor = Number(frm.inCondutor.value)
 
-    const valorMulta = Number(velPermitida + ( velPermitida * 0.20))
-
-    const valorMultaGrave = Number(valorMulta + (valorMulta * 0.20))
-
-
-
-
-
-}
-
-  /*  if(velCondutor === velPermitida || velCondutor < velPermitida){
-        resp.innerHTML = `Sem multa`
-
-    */
-   
-       
-    /*
-    if(velCondutor <= valorMultaLeve){
-        resp.innerHTML = `Multa leve`
-    }else if(velCondutor >= valorMultaLeve){
-        resp.innerHTML = `Multa Grave`
+    if(velCondutor <= velPermitida){
+        resp.innerHTML = `Situação: Sem multas`
+    }else if(velCondutor <= (velPermitida * 1.20)){
+        resp.innerHTML = `Situação: Multa leve`
     }else{
-        resp.innerHTML = `Sem multa`
+        resp.innerHTML = `Situação: Multa grave`
     }
-    */
-
-
-//velCondutor == velPermitida || velCondutor > velPermitida
+  
+})
