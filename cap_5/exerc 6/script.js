2a273730dcf26a4b30f97e3fb4ec79f0a8d814b6
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-01-21 19:01:51 -0300

    Javascript otimizado

--- cap_5/exerc 6/script.js
+++ cap_5/exerc 6/script.js
@@ -4,15 +4,16 @@ const resp = document.querySelector("h3")
 frm.addEventListener("submit", (e)=>{
     const numero = Number(frm.inNumero.value)
     e.preventDefault()
-    let numDivisores = 0 // Declara e inicializa contador
+    let temDivisor = 0 // Declara e inicializa contador
 
-    for (let i = 1; i<= numero; i++) {//vai percorrer todos os possíveis divisores do numero
+    for (let i = 2; i<= numero/2; i++) {//vai percorrer todos os possíveis divisores do numero
         if(numero % i == 0){//Verifica se i (1,2,3,4...) é divisor do numero
-            numDivisores++//se for divisor, incrementa contador
+            temDivisor = 1//muda o flag
+            break //sai da repetição
         }
         
     }
-    if(numDivisores == 2){//se possui apenas 2 divisores é primo
+    if(numero > 1 && !temDivisor){//se numero > 1 e não possui divisor
         resp.innerText = `${numero} é primo`
     }else{
         resp.innerText = `${numero} não é primo`
