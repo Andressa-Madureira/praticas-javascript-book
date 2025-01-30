7ea91c7d02da2e72e90ccf225cbaa46c3dd6f643
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-01-30 19:00:50 -0300

    Javascript finalizado

--- cap_5/exerc 7/script.js
+++ cap_5/exerc 7/script.js
@@ -1,10 +1,19 @@
 const frm = document.querySelector("form")
-const resp = document.querySelector("resp")
+const resp = document.querySelector("#resp")
 
 frm.addEventListener("submit", (e)=>{
     e.preventDefault()
 
     const numeroSimbolo = Number(frm.inNumero.value)
 
-    console.log(numeroSimbolo)
+    let estrelas = ""//variável que vai concatenar as estrelas/traços
+    for(let i = 1; i <= numeroSimbolo; i++){
+        if(i % 2 == 1){
+            estrelas = estrelas + "*" //na posição do ímpar do i: *
+        }else{
+            estrelas = estrelas + "-" //na posição par do i: -
+        }
+    }
+
+    resp.innerHTML = estrelas
 })
