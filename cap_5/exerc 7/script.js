826012cd2af11387441d83ba43dba32573f069ca
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-01-30 18:47:42 -0300

    Trocando de parágrafo para h3

--- cap_5/exerc 7/script.js
+++ cap_5/exerc 7/script.js
@@ -1,2 +1,10 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("resp")
+
+frm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    const numeroSimbolo = Number(frm.inNumero.value)
+
+    console.log(numeroSimbolo)
+})
